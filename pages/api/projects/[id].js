2d554f64e972d3983f45c502aa58f848b88dc8d3
zainabalazzawi <zainabalazzawi91@gmail.com>
2022-07-21 14:04:06 +0300

    Try to get single project

--- pages/api/projects/[id].js
+++ pages/api/projects/[id].js
@@ -2,6 +2,10 @@ import projects from "../../../data/Projects";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (req, res) {
+  // const {
+  //   query: { id = Number(req.params.id) },
+  // } = req;
+
   const id = Number(req.params.id);
   const project = projects.find((p) => p.id === id);
   if (project) {
@@ -10,6 +14,6 @@ export default function (req, res) {
   } else {
     res
       .status(400)
-      .json({ message: `that project doesn't exist ${req.params.id}` });
+      .json({ message: `that project doesn't exist ${req.params.id} ` });
   }
 }
