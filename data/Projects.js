e75391a8a964aae43f2fb4dad23d3c64af01ed10
Zainab Ahmad <zainab.ahmad@stadim.be>
2025-02-11 10:45:12 +0300

    add eventy project

--- data/Projects.js
+++ data/Projects.js
@@ -1,29 +1,39 @@
 const projects = [
   {
     id: 1,
+    projectName: "eventy",
+    description:
+      "This website about find Meetup events, join groups, or start your own. Make new friends and connect with like-minded people. Meet people near you who share your interests.",
+    githubLink: "https://github.com/zainabalazzawi/event-form",
+    link: "https://event-form-bice.vercel.app/",
+    librariesUsed: "javascript, next.js",
+    projectImage: "eventy.svg",
+  },
+  {
+    id: 2,
     projectName: "My Shopping Card",
     description:
-      "This site has interactive features that allow users to purchase products online,\n users can do the purchase process by clicking on the add button or clicking on the remove, pay button",
+      "This website has interactive features that allow users to purchase products online,\n users can do the purchase process by clicking on the add button or clicking on the remove, pay button",
     githubLink: "https://github.com/zainabalazzawi/my-shopping-cart",
     link: "https://my-shopping-cart-xi.vercel.app",
     librariesUsed: "javascript, react.js",
     projectImage: "project-1.svg",
   },
   {
-    id: 2,
+    id: 3,
     projectName: "SmoothieBucks",
     description:
-      "This site has interactive features that allow users to order smoothie drinks online; users can choose the flavor they like by clicking on the icon that Content the fruit they prefer to fill the cup and type their name using the input field",
+      "This website has interactive features that allow users to order smoothie drinks online; users can choose the flavor they like by clicking on the icon that Content the fruit they prefer to fill the cup and type their name using the input field",
     githubLink: "https://github.com/zainabalazzawi/smoothie-bucks",
     link: "https://smoothie-bucks.vercel.app/",
     librariesUsed: "javascript, react.js",
     projectImage: "project-2.svg",
   },
   {
-    id: 3,
+    id: 4,
     projectName: "Cake App",
     description:
-      "This site has interactive features that allow users to order cake & candy online; users can choose the flavor of the cake or the candy they like by clicking on the icon of the cake. Also, they can select the amount they want by clicking on the increment and decrement button",
+      "This website has interactive features that allow users to order cake & candy online; users can choose the flavor of the cake or the candy they like by clicking on the icon of the cake. Also, they can select the amount they want by clicking on the increment and decrement button",
     githubLink: "https://github.com/zainabalazzawi/my-app",
     link: "https://my-app-nine-phi.vercel.app/",
     librariesUsed: "javascript, react.js",
